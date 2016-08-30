62ee4e924a61d3d85b0ff89d552d11763b585112
Manoj Bableshwar <Manoj.Bableshwar@microsoft.com>
2016-08-30 19:30:29 +0530

    new tests

---
+++ FabrikamFiberService/AllPassingTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AllPassingTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Pass1()
+        {
+        }
+        [TestMethod]
+        public void Pass2()
+        {
+        }
+        [TestMethod]
+        public void Pass3()
+        {
+        }
+        [TestMethod]
+        public void Pass4()
+        {
+        }
+    }
+}
